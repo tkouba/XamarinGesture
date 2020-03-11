62c94f2c1cb17d3e273a39da27465379ff85747a
Tomas Kouba <Tomas.Kouba@sntcz.cz>
2020-03-11 18:31:45 +0100

    Add project files.

---
+++ XamarinGesture/XamarinGesture.WPF/GestureLabelRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+using XamarinGesture.WPF;
+
+//[assembly: ExportRenderer(typeof(Label), typeof(GestureLabelRenderer))]
+namespace XamarinGesture.WPF
+{
+    public class GestureLabelRenderer : LabelRenderer
+    {
+        private static readonly Action EmptyDelegate = delegate { };
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (AutoTrack)
+            {
+                if (Tracker != null)
+                    Tracker.Dispose();
+                Tracker = new WPFVisualElementTracker<Label, FrameworkElement> { Element = Element, Control = Control };
+            }
+        }
+
+    }
+}
+
---
+++ XamarinGesture/XamarinGesture/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinGesture
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int counter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var tapGesture = new TapGestureRecognizer();
+            tapGesture.Tapped += OnTapped;
+            contentLabel.GestureRecognizers.Add(tapGesture);
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            contentLabel.GestureRecognizers.Add(panGesture);
+
+            var pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += OnPinchUpdated;
+            contentLabel.GestureRecognizers.Add(pinchGesture);
+        }
+
+        void OnTapped(object sender, EventArgs e)
+        {
+            contentLabel.Text = $"[{counter++}] Tap";
+        }
+
+        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            contentLabel.Text = $"[{counter++}] Pan: {e.StatusType} id={e.GestureId} total x={e.TotalX} y={e.TotalY}";
+        }
+
+        void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            contentLabel.Text = $"[{counter++}] Pinch: {e.Status} scale={e.Scale} origin x={e.ScaleOrigin.X} y={e.ScaleOrigin.Y}";
+        }
+    }
+}
